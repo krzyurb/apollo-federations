17d64cf736b3f6e1c12cb1509c600adbb1bff2e0
krzyurb <11517908+krzyurb@users.noreply.github.com>
2019-10-09 11:31:43 +0200

    Attach context to microservice

--- packages/core/src/microservice.ts
+++ packages/core/src/microservice.ts
@@ -2,6 +2,7 @@ import { ApolloServer } from "apollo-server";
 import { buildFederatedSchema } from "@apollo/federation";
 import { DocumentNode } from "graphql";
 import { GraphQLResolverMap } from "apollo-graphql";
+import { Context } from "apollo-server-core";
 
 export interface IMicroserviceConfig {
   name: string;
@@ -16,13 +17,15 @@ export default class Microservice {
     schema: DocumentNode,
     resolvers: GraphQLResolverMap<any>,
     config: IMicroserviceConfig,
+    context?: Context,
   ) {
     this.config = config;
-    this.server = this.buildServer(schema, resolvers);
+    this.server = this.buildServer(schema, resolvers, context);
   }
 
-  private buildServer(schema, resolvers) {
+  private buildServer(schema, resolvers, context) {
     return new ApolloServer({
+      context,
       schema: buildFederatedSchema([{
         typeDefs: schema,
         resolvers,
